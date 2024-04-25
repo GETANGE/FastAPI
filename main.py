c8da04c9fe7e8a99ce4d12bd3e377bc8de942d3f
Getange <emmanuelgetange48@gmail.com>
2024-04-25 16:28:00 +0300

    ADD:last commit

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["*"], 
     allow_headers=["*"],
 )
 
